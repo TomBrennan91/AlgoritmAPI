e338e5c52b290fe652e5075ef5b843154e49f687
Tom Brennan <tom-brennan@hotmail.co.uk>
2025-07-15 10:46:36 +0100

    All Tests passing

--- poker/poker.py
+++ poker/poker.py
@@ -13,44 +13,56 @@ class HandType(Enum):
     FOUR = 8
     STRAIGHT_FLUSH = 9
 
-def best_hand(hands_str: list[str]) -> str:
+def best_hand(hands_str: list[str]) -> list[str]:
     """
     :param hands_str: a list of strings each representing a set of five playing cards
     :return: The winning poker hand from the given hands
     """
     hands = {}
+    best_hand_type = HandType.HIGH_CARD.value
     for hand_str in hands_str:
         card_strs = hand_str.split(" ")
+        hand = []
+        for card_str in card_strs:
+            hand.append(Card(card_str))
         hand = list(map(Card, card_strs))
         hand.sort(key=lambda card: card.rank)
         hand_type = get_hand_type(hand)
-        hands[hand] = hand_type
+        if int(hand_type.value) > best_hand_type:
+            best_hand_type = hand_type
 
+        hands[hand_str] = [hand_type, hand]
 
+    best_hands = {k: v for k, v in hands.items() if v[0].value == best_hand_type}
 
-    best_hand_type = max(hands.values())
-    best_hands = [k for k, v in hands.items() if v == best_hand_type]
+    highest_ranked_best_hands = []
+    for high_card_idx in range (4, -1, -1):
+        # if there are multiple each of the highest value type, return the one with the highest rank.
 
-    if len(best_hands) == 1:
-        return best_hands[0]
+        highest_card = 0
+        highest_ranked_best_hands = []
+        for hand_str, hand in best_hands.items():
+            if hand[1][high_card_idx].rank > highest_card:
+                highest_card = hand[1][high_card_idx].rank
+                highest_ranked_best_hands = [hand_str]
+            elif hand[1][high_card_idx].rank == highest_card:
+                highest_ranked_best_hands.append(hand_str)
 
+        best_hands = {k: v for k, v in best_hands.items() if k in highest_ranked_best_hands}
 
-    # if there are multiple each of the highest value type, return the one with the highest rank.
-    highest_card = 0
-    highest_ranked_best_hand = None
-    for joint_best_hand in best_hands:
-        if joint_best_hand[4].rank > highest_card:
-            highest_card = joint_best_hand[4].rank
-            highest_ranked_best_hand = joint_best_hand
+        if len(highest_ranked_best_hands) == 1:
+            return highest_ranked_best_hands
 
-    return highest_ranked_best_hand
+    return highest_ranked_best_hands
 
 
 def get_hand_type(hand) -> HandType:
     if is_flush(hand) and is_straight(hand):
         return HandType.STRAIGHT_FLUSH
 
-    ranks = list(map(Card.rank, hand))
+    ranks = []
+    for card in hand:
+        ranks.append(card.rank)
     rank_count = Counter(ranks)
 
     if rank_count[0] == 4:
